2c1590b718a443a15db9a0e76d8b946e8aeaab92
Unknown <jakeweiner@outlook.com>
2019-06-11 20:48:54 +1000

    Djikstras Changed

--- LAPSO_Req/djikstra.h
+++ LAPSO_Req/djikstra.h
@@ -10,26 +10,22 @@ using namespace std;
 using namespace LaPSO;
 
 typedef pair<int, int> Edge;
-typedef std::vector<Edge> EdgeVec;
 typedef map<Edge,int> Edge_Int_Map;
-typedef vector<int> vi;
-typedef vector<double,int> di;  
-typedef vector<Edge> EdgeVec;
 typedef vector<string> split_vector_type;
+typedef pair<int, int> NodeEdgePair;
 
 
+double djikstras(double max_perturb, const Edge_Int_Map& EIM,
+		 const vector<vector<NodeEdgePair>>& node_neighbours, int start, int end,
+		 vector<NodeEdgePair>& parents, int num_nodes, int num_edges, 
+		 DblVec& rc, IntVec& x, int comm_idx, int num_comm,double thresh=1e99);
 
-double djikstras(double max_perturb, Edge_Int_Map& EIM,
-    map<int, map<int, bool>>& node_neighbours, int start, int end,
-    vector<int>& parents, int num_nodes, int num_edges, 
-    DblVec& rc, IntVec& x, int comm_idx, int num_comm);
+double djikstras_naive(const Edge_Int_Map& EIM,
+		       const vector<vector<NodeEdgePair>>& node_neighbours, int start, int end,
+		       vector<NodeEdgePair>& parents, int num_nodes, int num_edges, 
+		       DblVec& rc, IntVec& x, int comm_idx, int num_comm,double thresh=1e99);
 
-double djikstras_naive(Edge_Int_Map& EIM,
-    map<int, map<int, bool>>& node_neighbours, int start, int end,
-    vector<int>& parents, int num_nodes, int num_edges, 
-    DblVec& rc, IntVec& x, int comm_idx, int num_comm);
-
-double djikstras_naive_cutSet(Edge_Int_Map& EIM,
-    map<int, map<int, bool>>& node_neighbours, int start, int end,
-    vector<int>& parents, int num_nodes, int num_edges,
-      DblVec& rc, IntVec& x, int comm_idx, int num_comm,map<int,bool>& S_cutSet,map<int,bool>& T_cutSet, float thresh);
+double djikstras_naive_cutSet(const Edge_Int_Map& EIM,
+    const vector<vector<NodeEdgePair>>& node_neighbours, int start, int end,
+    vector<NodeEdgePair>& parents, int num_nodes, int num_edges,
+    DblVec& rc, IntVec& x, int comm_idx, int num_comm,vector<bool>& S_cutset, double thresh);
--- djikstra.cpp
+++ djikstra.cpp
@@ -9,14 +9,17 @@ using namespace LaPSO;
 struct node {
     int idx;
     pair<double, int> properties;
-    double epsilon;
+    //double epsilon;
    
 };
 
+constexpr bool operator>(const pair<double,int> &A,const pair<double,int> &B){
+  return (A.first > B.first) || (A.first==B.first && A.second > B.second);
+}
+
 struct node_naive {
     int idx;
-    double dist;
-   
+    double dist;   
 };
 
 // STRUCT TEMPLATE used for comparison operator in priority_queue // what am i comparing? a node which will have
@@ -24,34 +27,35 @@ struct node_naive {
 struct mygreater { // functor for operator>
     constexpr bool operator()(const node& _Left, const node& _Right)
     { // apply operator> to operands
-        return (_Left.properties.first > (_Right.properties.first + _Right.epsilon) || ((_Left.properties.first == _Right.properties.first) && (_Left.properties.second > _Right.properties.second)));
+      // AE: should be consistent on use of epsilon (either use it also for equality or not for >
+      return _Left.properties > _Right.properties;
+      //return (_Left.properties.first > (_Right.properties.first /*+ _Right.epsilon*/) || ((_Left.properties.first == _Right.properties.first) && (_Left.properties.second > _Right.properties.second)));
     }
 };
+bool operator>(const node& Left,const node& Right){ return mygreater()(Left,Right); }
 
-double djikstras(double max_perturb, Edge_Int_Map& EIM,
-    map<int, map<int, bool>>& node_neighbours, int start, int end,
-    vector<int>& parents, int num_nodes, int num_edges,
-      DblVec& rc, IntVec& x, int comm_idx, int num_comm)
+double djikstras(double max_perturb, const Edge_Int_Map& EIM,
+    const vector<vector<NodeEdgePair>>&  node_neighbours, int start, int end,
+    vector<NodeEdgePair>& parents, int num_nodes, int num_edges,
+		 DblVec& rc, IntVec& x, int comm_idx, int num_comm,double thresh)
 {
     //vector<pair<double, int>> dist; // for storing the distance of every other node from source.
     //vector<int> parents; // predecessor nodes
     priority_queue<node, vector<node>, mygreater> Q; // min heap
-    vector<pair<double,int>> distances;
-    int current_edge_idx;
-    double edge_weight;
-    int highest_node_idx = node_neighbours.rbegin()->first;
-    for (int i=0; i<highest_node_idx+1; i++){
-        distances.push_back({INF,0});
-    }
-    map <int,bool> visited;
-    parents.assign(highest_node_idx+1, -1);
+    vector<pair<double,int>> distances(num_nodes,{thresh,0});
+    
+    vector<bool> visited(num_nodes,false);
+    if(parents.size() < num_nodes)
+	parents.resize(num_nodes,NodeEdgePair(-1,-1));
+    else
+	std::fill(parents.begin(),parents.end(),NodeEdgePair(-1,-1));
     distances[start].first = 0;
 
-    Q.push({start,{0.0,0},max_perturb*num_edges});
+    Q.push({start,{0.0,0}/*,max_perturb*num_edges*/});
     while (!Q.empty()) {
         node current_node = Q.top();
 
-        if (current_node.idx == end) {
+        if (current_node.idx == end || distances[current_node.idx].first >= thresh) {
             /*
             int last_node = end;
             while (parents[last_node] != -1) {
@@ -60,65 +64,42 @@ double djikstras(double max_perturb, Edge_Int_Map& EIM,
                 last_node = parents[last_node];
             }
             */
-           return current_node.properties.first;
+           return distances[end].first;
         }
         
         Q.pop();
-        visited[current_node.idx] = 1;
+	if(visited[current_node.idx]) continue;
+        visited[current_node.idx] = true;
         //cout << "node " << current_node.idx << " popped" << endl;
-        //iterate through neighbours of current_node
-        if (node_neighbours.find(current_node.idx) != node_neighbours.end()) {
-            //it refers to each <int,bool> pair
-            for (map<int, bool>::iterator it = node_neighbours[current_node.idx].begin();
-                 it != node_neighbours[current_node.idx].end(); it++) {
+        //iterate through neighbours of current_node 
+	for(const NodeEdgePair adj : node_neighbours[current_node.idx] ){
                 
-                int neighbour_node = it->first; 
-                // has node been visited
-                if (visited.find(neighbour_node) != visited.end()){
-                    continue;
-                }
-                int edge_used = 0;
-                /*
-                cout << "current node " << current_node.idx << endl;
-                cout << "neighbour node " << neighbour_node << endl;
-                */
-                Edge current_edge = Edge(current_node.idx, neighbour_node);
-                if (EIM.find(current_edge) != EIM.end()) {
-                    current_edge_idx = EIM[current_edge];
-                }
-                else{
-                    cerr << "edge not found in EIM --> djikstras" << endl;
-                    return -1;
-                }
-                //cout << "current edge idx " << current_edge_idx << endl;
-                edge_weight = rc[current_edge_idx +  comm_idx*num_edges];
-                //cout << "edge weight is " << edge_weight << endl;
-                for (int i=0; i< num_comm; i++){
-                    if(x[current_edge_idx + i*num_edges] == 1){
-                        edge_used += 1;
-                    }
-                }
-
-                // if distance of node is shorter, or if distances is same but violation is less
-                    if ( (distances[neighbour_node].first > (distances[current_node.idx].first + edge_weight))
-                    || ( (distances[neighbour_node].first == (distances[current_node.idx].first + edge_weight)) &&
-                       (distances[neighbour_node].second > (distances[current_node.idx].second + edge_used) ) ) ) {
-
-                           distances[neighbour_node].first = distances[current_node.idx].first + edge_weight;
-                           distances[neighbour_node].second = distances[current_node.idx].second + edge_used;
-                           parents[neighbour_node] = current_node.idx;
-                           Q.push({neighbour_node, {distances[neighbour_node].first,distances[neighbour_node].second}, max_perturb*num_edges});
-                           //cout << "node " << neighbour_node << " pushed - dist " << distances[neighbour_node].first
-                           //<< " viol - " << distances[neighbour_node].second << endl;
-                    }
-            }
-        } else {
-            cerr << "node not found in neighbours list --> djikstras" << endl;
-            return -1;
-        }
+	    const int neighbour_node = adj.first;
+	    if(visited[neighbour_node]) continue;
+	    const int edge = adj.second;
+	    const double edge_weight = rc[edge +  comm_idx*num_edges];
+	    auto d = distances[current_node.idx];
+	    d.first += edge_weight;
+	    if(d.first >= thresh) continue; // too long to consider
+	    int edge_used = 0;
+	    for (int i=0; i< num_comm; i++){
+		if(x[edge + i*num_edges] == 1)
+		    edge_used += 1;
+	    }
+	    d.second += edge_used;
+	    
+	    // if distance of node is shorter, or if distances is same but violation is less
+	    if ( distances[neighbour_node] > d ) {		
+	      distances[neighbour_node] = d;
+	      parents[neighbour_node] = NodeEdgePair(current_node.idx,edge);
+	      Q.push({neighbour_node, {d.first,d.second}/*, max_perturb*num_edges*/});
+	      //cout << "node " << neighbour_node << " pushed - dist " << distances[neighbour_node].first
+	      //<< " viol - " << distances[neighbour_node].second << endl;
+	    }
 	}
+    }
     
-    return -1;
+    return distances[end].first;
 }
 
 
@@ -130,27 +111,26 @@ struct mygreater_naive
 			}
 		};
 
-double djikstras_naive(Edge_Int_Map& EIM,
-    map<int, map<int, bool>>& node_neighbours, int start, int end,
-    vector<int>& parents, int num_nodes, int num_edges,
-      DblVec& rc, IntVec& x, int comm_idx, int num_comm){
+double djikstras_naive(const Edge_Int_Map& EIM, // unused
+		       const vector<vector<NodeEdgePair>> &node_neighbours, int start, int end,
+		       vector<NodeEdgePair>& parents, int num_nodes, int num_edges,
+		       DblVec& rc, IntVec& x, int comm_idx, int num_comm,double thresh){
 
     priority_queue<node_naive, vector<node_naive>, mygreater_naive> Q; // min heap
-    vector<double> distances;
-    int current_edge_idx;
-    double edge_weight;
-    int highest_node_idx = node_neighbours.rbegin()->first;
+    vector<bool> visited(num_nodes,false);
+    vector<double> distances(num_nodes,thresh);
     
-    map <int,bool> visited;
-    parents.assign(highest_node_idx+1, -1);
-    distances.assign(highest_node_idx+1,INF);
+    if(parents.size() < (size_t)num_nodes)
+	parents.resize(num_nodes,NodeEdgePair(-1,-1));
+    else
+	std::fill(parents.begin(),parents.end(),NodeEdgePair(-1,-1));
     distances[start] = 0.0;
-
+    
     Q.push({start,0.0});
     while (!Q.empty()) {
         node_naive current_node = Q.top();
 
-        if (current_node.idx == end) {
+        if (current_node.idx == end || distances[current_node.idx] >= thresh) {
             /*
             int last_node = end;
             while (parents[last_node] != -1) {
@@ -159,135 +139,82 @@ double djikstras_naive(Edge_Int_Map& EIM,
                 last_node = parents[last_node];
             }
             */
-           return current_node.dist;
+	    return distances[end]; 
         }
         
         Q.pop();
-        visited[current_node.idx] = 1;
+	if(visited[current_node.idx]) continue;
+	visited[current_node.idx] = true;
         //cout << "node " << current_node.idx << " popped" << endl;
         //iterate through neighbours of current_node
-        if (node_neighbours.find(current_node.idx) != node_neighbours.end()) {
-            //it refers to each <int,bool> pair
-            for (map<int, bool>::iterator it = node_neighbours[current_node.idx].begin();
-                 it != node_neighbours[current_node.idx].end(); it++) {
+	for(const NodeEdgePair adj : node_neighbours[current_node.idx] ){
                 
-                int neighbour_node = it->first; 
-                // has node been visited
-                if (visited.find(neighbour_node) != visited.end()){
-                    continue;
-                }
-            
-               
-                Edge current_edge = Edge(current_node.idx, neighbour_node);
-                if (EIM.find(current_edge) != EIM.end()) {
-                    current_edge_idx = EIM[current_edge];
-                }
-                else{
-                    cerr << "edge not found in EIM --> djikstras" << endl;
-                    return -1;
-                }
-                edge_weight = rc[current_edge_idx +  comm_idx*num_edges];
-
-               
-
-                // if distance of node is shorter, or if distances is same but violation is less
-                    if ( distances[neighbour_node] > (distances[current_node.idx] + edge_weight)){
-                        distances[neighbour_node] = distances[current_node.idx] + edge_weight;
-                      
-                        parents[neighbour_node] = current_node.idx;
-                        Q.push({neighbour_node, distances[neighbour_node]});
-                        //cout << "node " << neighbour_node << " pushed - dist " << distances[neighbour_node].first
-                        //<< " viol - " << distances[neighbour_node].second << endl;
-                    }
-            }
-        } else {
-            cerr << "node not found in neighbours list --> djikstras" << endl;
-            return -1;
-        }
+	    const int neighbour_node = adj.first;
+	    if(visited[neighbour_node]) continue;
+	    const int edge = adj.second;
+	    const double d = distances[current_node.idx]+rc[edge +  comm_idx*num_edges];
+	    if(d >= thresh) continue;
+	    // if distance of node is shorter, or if distances is same but violation is less
+	    if ( distances[neighbour_node] > d){
+		distances[neighbour_node] = d;
+		parents[neighbour_node] = NodeEdgePair(current_node.idx,edge); 
+		Q.push({neighbour_node, d});
+		//cout << "node " << neighbour_node << " pushed - dist " << distances[neighbour_node].first
+		//<< " viol - " << distances[neighbour_node].second << endl;
+	    }
 	}
+    }
     
-    return -1;
+    return distances[end];
 }
 
-double djikstras_naive_cutSet(Edge_Int_Map& EIM,
-    map<int, map<int, bool>>& node_neighbours, int start, int end,
-    vector<int>& parents, int num_nodes, int num_edges,
-      DblVec& rc, IntVec& x, int comm_idx, int num_comm,map<int,bool>& S_cutSet,map<int,bool>& T_cutSet, float thresh){
+double djikstras_naive_cutSet(const Edge_Int_Map& EIM,
+			      const vector<vector<NodeEdgePair>> &node_neighbours, int start, int end,
+			      vector<NodeEdgePair>& parents, int num_nodes, int num_edges,
+			      DblVec& rc, IntVec& x, int comm_idx, int num_comm,vector<bool>& S_cutSet, double thresh){
 
     priority_queue<node_naive, vector<node_naive>, mygreater_naive> Q; // min heap
-    vector<double> distances;
-    int current_edge_idx;
-    double edge_weight;
-    int highest_node_idx = node_neighbours.rbegin()->first;
+    vector<double> distances(num_nodes,thresh);
+    vector<bool> visited(num_nodes,false);
     
-    map <int,bool> visited;
-    parents.assign(highest_node_idx+1, -1);
-    distances.assign(highest_node_idx+1,INF);
+    if(parents.size() < num_nodes)
+	parents.resize(num_nodes,NodeEdgePair(-1,-1));
+    else
+	std::fill(parents.begin(),parents.end(),NodeEdgePair(-1,-1));
     distances[start] = 0.0;
 
     Q.push({start,0.0});
     while (!Q.empty()) {
         node_naive current_node = Q.top();
-
+	if(distances[current_node.idx] >= thresh) break;
         Q.pop();
-        visited[current_node.idx] = 1;
+	if(visited[current_node.idx]) continue;
+        visited[current_node.idx] = true;
         //cout << "node " << current_node.idx << " popped" << endl;
         //iterate through neighbours of current_node
-        if (node_neighbours.find(current_node.idx) != node_neighbours.end()) {
-            //it refers to each <int,bool> pair
-            for (map<int, bool>::iterator it = node_neighbours[current_node.idx].begin();
-                 it != node_neighbours[current_node.idx].end(); it++) {
+	for(const NodeEdgePair adj : node_neighbours[current_node.idx] ){
                 
-                int neighbour_node = it->first; 
-                // has node been visited
-                if (visited.find(neighbour_node) != visited.end()){
-                    continue;
-                }
-            
-               
-                Edge current_edge = Edge(current_node.idx, neighbour_node);
-                if (EIM.find(current_edge) != EIM.end()) {
-                    current_edge_idx = EIM[current_edge];
-                }
-                else{
-                    cerr << "edge not found in EIM --> djikstras" << endl;
-                    return -1;
-                }
-                edge_weight = rc[current_edge_idx +  comm_idx*num_edges];
-
-               
-
-                // if distance of node is shorter, or if distances is same but violation is less
-                    if ( distances[neighbour_node] > (distances[current_node.idx] + edge_weight)){
-                        distances[neighbour_node] = distances[current_node.idx] + edge_weight;
+	    const int neighbour_node = adj.first;
+	    if(visited[neighbour_node]) continue;
+	    const int edge = adj.second;
+	    const double d = distances[current_node.idx] + rc[edge +  comm_idx*num_edges];               
+	    
+	    // if distance of node is shorter, or if distances is same but violation is less
+	    if ( distances[neighbour_node] > d){
+		distances[neighbour_node] = d;
                       
-                        parents[neighbour_node] = current_node.idx;
-                        Q.push({neighbour_node, distances[neighbour_node]});
-                        //cout << "node " << neighbour_node << " pushed - dist " << distances[neighbour_node].first
-                        //<< " viol - " << distances[neighbour_node].second << endl;
-                    }
-            }
-        } else {
-            cerr << "node not found in neighbours list --> djikstras" << endl;
-            return -1;
-        }
+		parents[neighbour_node] = NodeEdgePair(current_node.idx,edge);
+		Q.push({neighbour_node, d});
+		//cout << "node " << neighbour_node << " pushed - dist " << distances[neighbour_node].first
+		//<< " viol - " << distances[neighbour_node].second << endl;
+	    }
 	}
-    
-    // at this point, any nodes with distanes greater than threshold mean they can't be reached
-
-    for (vector<double>::iterator it = distances.begin(); it!= distances.end(); it++){
-        int index =  distance(distances.begin(),it);
-        if ((*it) <thresh){
-            if (S_cutSet.find(index)==S_cutSet.end()){
-                S_cutSet[index] = true;
-            }    
-        }
-        else{
-            if (T_cutSet.find(index)==T_cutSet.end()){
-                T_cutSet[index] = true;
-            }    
-        }
     }
+    // at this point, any nodes with distances greater than threshold mean they can't be reached
+    if(S_cutSet.size() < num_nodes)
+	S_cutSet.resize(num_nodes,false);
+    for(int i=0;i<num_nodes;++i)
+	S_cutSet[i] = (distances[i] < thresh);
 
     return distances[end];
 }
